abcb26c2770e749dcdc23d67b90bcdb64e9cf831
Vlad Calomfirescu <vlad.calomfirescu@gmail.com>
2025-04-13 13:18:51 +0300

    implement ping

--- DPDK/dpdk-app/traffic_engine.c
+++ DPDK/dpdk-app/traffic_engine.c
@@ -1,16 +1,23 @@
+// traffic_engine.c
 #include "include/eth.h"
+#include "include/icmp.h"
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
     uint16_t port_id = 0;
     int is_tx = 0;
+    enum protocol_type proto = PROTO_ETH;
 
-    // Detect role: TX or RX
+    // Parse CLI: ./traffic_engine [tx|rx] [eth|icmp]
     if (argc > 1 && strcmp(argv[1], "tx") == 0)
         is_tx = 1;
 
-    // Initialize EAL
+    if (argc > 2) {
+        if (strcmp(argv[2], "icmp") == 0) proto = PROTO_ICMP;
+        else if (strcmp(argv[2], "eth") == 0) proto = PROTO_ETH;
+    }
+
     int ret = rte_eal_init(argc, argv);
     if (ret < 0)
         rte_exit(EXIT_FAILURE, "Failed to initialize EAL\n");
@@ -18,15 +25,19 @@ int main(int argc, char *argv[]) {
     argc -= ret;
     argv += ret;
 
-    // Initialize Ethernet and create mempool
     struct rte_mempool *mbuf_pool = eth_init(port_id);
-    if (!mbuf_pool)
-        return 1;
+    if (!mbuf_pool) return 1;
 
-    if (is_tx) {
-        eth_tx_loop(port_id, mbuf_pool);
+    if (proto == PROTO_ICMP) {
+        if (is_tx)
+            icmp_tx_loop(port_id, mbuf_pool);
+        else
+            icmp_rx_loop(port_id);
     } else {
-        eth_rx_loop(port_id);
+        if (is_tx)
+            eth_tx_loop(port_id, mbuf_pool);
+        else
+            eth_rx_loop(port_id);
     }
 
     return 0;
